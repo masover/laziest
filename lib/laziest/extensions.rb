f12444e7cd5dcdb4a99d54784bd4d77131a4a798
David Masover <masover@iastate.edu>
2013-07-09 19:35:49 -0500

    Added chunk.

--- lib/laziest/extensions.rb
+++ lib/laziest/extensions.rb
@@ -7,13 +7,76 @@ module Laziest
             y << 1
           end
         end
-        Laziest::Counter.new enum
+        Counter.new enum
       end
       def to_a
-        Laziest::ArrayPromise.new self 
+        ArrayPromise.new self 
       end
       def group_by &block
-        Laziest::Group.new self, &block
+        Group.new self, &block
+      end
+
+      def chunk state=nil
+        Enumerator.new do |yield_array|
+          # Should give us a new, rewound copy.
+          # ec = enum copy
+          ec = enum_for(:each)
+          array = nil
+          current_result = nil
+          lazy_array = nil
+
+          # first iteration, to set things up.
+          begin
+            value = nil
+            while current_result.nil? || current_result == :_separator
+              value = ec.next
+              current_result = state.nil? ? (yield value) : (yield value, state)
+            end
+            array = [value]
+          rescue StopIteration
+            array = nil
+          end
+
+          loop do
+            break if array.nil? # either we're empty at start or we're done.
+
+            # An enumerator that returns all following values of this array.
+            array_promise_enum = Enumerator.new do |yield_element|
+              loop do
+                value = nil
+                begin
+                  value = ec.next
+                  result = state.nil? ? (yield value) : (yield value, state)
+                  # is this a new value?
+                  if result != current_result || result == :_alone
+                    current_result = result
+                    while current_result.nil? || current_result == :_separator
+                      value = ec.next
+                      current_result = state.nil? ? (yield value) : (yield value, state)
+                    end
+                    array = [value]
+                    break
+                  end
+                rescue StopIteration
+                  # We're done. Clean up.
+                  array = nil
+                  current_result = nil
+                  lazy_array = nil
+                  break
+                end
+                yield_element << value
+              end
+            end
+
+            # nil_on_empty is false, because we will _always_ have elements.
+            lazy_array = ArrayPromise.new array_promise_enum, array
+
+            yield_array << [current_result, lazy_array]
+
+            # Now we have control back. Before we loop again, finish the previous array.
+            lazy_array.__force__
+          end
+        end
       end
     end
   end
